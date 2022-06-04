0044577f6b01d2b945ca2980b8d7e76f6fbcb182
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-06-04 19:56:27 +0200

    Add example

---
+++ Examples/GetLogs.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\IISParser.psd1 -Force
+
+Get-IISParsedLog -FilePath "C:\Support\GitHub\IISParser\Ignore\u_ex220507.log" | Select-Object -First 5 | Format-Table
+Get-IISParsedLog -FilePath "C:\Support\GitHub\IISParser\Ignore\u_ex220507.log" | Select-Object -Last 5 | Format-Table
+Get-IISParsedLog -FilePath "C:\Support\GitHub\IISParser\Ignore\u_ex220507.log" -First 5 -Last 5 -Skip 1 | Format-Table
