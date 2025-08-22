c08e0d9cfa78543f17dce799c403817b8b4f3a5f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-22 21:27:56 +0200

    Add missing XML documentation

--- IISParser/Utils.cs
+++ IISParser/Utils.cs
@@ -3,7 +3,15 @@ using System.IO;
 
 namespace IISParser;
 
+/// <summary>
+/// Provides helper methods used by the parser.
+/// </summary>
 public static class Utils {
+    /// <summary>
+    /// Reads all lines from a file allowing for shared read access.
+    /// </summary>
+    /// <param name="file">The path to the file to read.</param>
+    /// <returns>A list containing each line of the file.</returns>
     public static List<string> ReadAllLines(string file) {
         var lines = new List<string>();
         using var fileStream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
