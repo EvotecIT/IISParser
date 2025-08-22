317d1c2f7bf08b3592733319b78de5f9882a24ec
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-22 21:59:58 +0200

    Support expanding fields only on demand

--- Module/Tests/Get-IISParsedLog.Tests.ps1
+++ Module/Tests/Get-IISParsedLog.Tests.ps1
@@ -1,10 +1,16 @@
 Describe 'Get-IISParsedLog' {
-    It 'parses log file' {
+    It 'returns native object by default' {
         $logPath = (Resolve-Path "$PSScriptRoot/../../IISParser.Tests/TestData/sample.log").Path
         $result = Get-IISParsedLog -FilePath $logPath
         $result.csUriStem | Should -Be '/index.html'
         $result.scStatus | Should -Be 200
-        $result.'X-Forwarded-For' | Should -Be '192.168.0.1'
+        ($result.PSObject.Properties.Match('X-Forwarded-For').Count) | Should -Be 0
         $result.Fields['X-Forwarded-For'] | Should -Be '192.168.0.1'
+    }
+
+    It 'expands fields when requested' {
+        $logPath = (Resolve-Path "$PSScriptRoot/../../IISParser.Tests/TestData/sample.log").Path
+        $result = Get-IISParsedLog -FilePath $logPath -Expand
+        $result.'X-Forwarded-For' | Should -Be '192.168.0.1'
     }
 }
