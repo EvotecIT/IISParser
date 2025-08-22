b40dbd61ca9fe72d738cc3b8652450bec75ff131
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-22 22:14:37 +0200

    Handle large IIS log values

--- IISParser.Tests/ParserEngineTests.cs
+++ IISParser.Tests/ParserEngineTests.cs
@@ -17,4 +17,14 @@ public class ParserEngineTests {
         Assert.Equal(200, evt.scStatus);
         Assert.Equal("192.168.0.1", evt.Fields["X-Forwarded-For"]);
     }
+
+    [Fact]
+    public void ParseLog_HandlesValuesAboveIntMax() {
+        var path = Path.Combine(AppContext.BaseDirectory, "TestData", "large_values.log");
+        var engine = new ParserEngine(path);
+        var evt = engine.ParseLog().Single();
+        Assert.Equal(3000000000L, evt.scBytes);
+        Assert.Equal(4000000000L, evt.csBytes);
+        Assert.Equal(5000000000L, evt.timeTaken);
+    }
 }
